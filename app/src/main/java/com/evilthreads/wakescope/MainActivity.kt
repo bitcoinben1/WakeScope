6f40fe01a31ed41c03e3bc2bb64d7921d1ccb539
Chris Basinger <evilthreads669966@gmail.com>
2020-10-06 01:08:44 -0500

    WakeScope library

---
+++ app/src/main/java/com/evilthreads/wakescope/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.evilthreads.wakescope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.evilthreads.wakescopelib.wakeScope
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        wakeScope {
+            Log.d("WAKESCOPE", "PROCESSOR STAYS AWAKE IN THIS SCOPE")
+        }
+    }
+}
